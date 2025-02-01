a47d8e8e26df9d33a78558da5793cdffa633452a
Abdullah (Teo) Kazimov <89588544+abdullahkazimov@users.noreply.github.com>
2025-02-01 08:29:19 +0400

    Add files via upload

---
+++ handling-form-submission/src/main/java/com/example/handlingformsubmission/Greeting.java
@@ -0,0 +1,23 @@
+package com.example.handlingformsubmission;
+
+public class Greeting {
+
+    private Long id;
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
